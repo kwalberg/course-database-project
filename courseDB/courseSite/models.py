f5281e90712d4254199ad26c892394891a184365
Eric Huang <ehuang@macalester.edu>
2018-02-19 16:12:37 -0600

    Basic DB models

--- courseDB/courseSite/models.py
+++ courseDB/courseSite/models.py
@@ -1,10 +1,34 @@
 from django.db import models
 
 # Create your models here.
-class Class(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    department = models.CharField(max_length=5)
-    number = models.IntegerField(default=0)
 
-    def __unicode__(self):
-        return self.name
+class Course(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    instructor = models.CharField(max_length=150)
+
+    DURATION_CHOICES = [('1', '2 Weeks'), ('2', '8 Weeks')]
+    duration = models.CharField(max_length=7, choices=DURATION_CHOICES)
+
+    # Don't require file upload
+    image = models.ImageField('course_art/', null=True, blank=True)
+
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['title','description','instructor','duration','image']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Name it something good'}),
+            'description': forms.Textarea(attrs={'rows':3}),
+            'instructor': forms.TextInput(attrs={'placeholder': 'Django Guru'}),
+        }
+        labels = {
+            'title': 'Course Title',
+            'image': 'Art',
+        }
+        help_texts = {
+            'description': 'Provide some information about what students will learn.',
+        }
+
+
