71c186d0f01e91d6781dbe04891ebe86055cfed9
venkatsomala <lsomala@macalester.edu>
2018-02-22 17:02:03 -0600

    search bar

--- courseDB/courseSite/models.py
+++ courseDB/courseSite/models.py
@@ -7,27 +7,14 @@ class Course(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     instructor = models.CharField(max_length=150)
-    # DURATION_CHOICES = [('1', '2 Weeks'), ('2', '8 Weeks')]
-    # duration = models.CharField(max_length=7, choices=DURATION_CHOICES)
+
+    DURATION_CHOICES = [('1', '2 Weeks'), ('2', '8 Weeks')]
+    duration = models.CharField(max_length=7, choices=DURATION_CHOICES)
+
+    def __str__(self):
+        return self.title
+
     # Don't require file upload
     #image = models.ImageField('course_art/', null=True, blank=True)
-
-'''
-class CourseForm(forms.ModelForm):
-    class Meta:
-        model = Course
-        fields = ['title','description','instructor','duration','image']
-        widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Name it something good'}),
-            'description': forms.Textarea(attrs={'rows':3}),
-            'instructor': forms.TextInput(attrs={'placeholder': 'Django Guru'}),
-        }
-        labels = {
-            'title': 'Course Title',
-            'image': 'Art',
-        }
-        help_texts = {
-            'description': 'Provide some information about what students will learn.',
-        }'''
 
 
--- courseDB/courseSite/views.py
+++ courseDB/courseSite/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from .models import Course
+
 
 # Create your views here.
 def index(request):
     return render(request,'searchBar.html',context={})
 
 def class_page(request, class_name):
-    return render(request, 'classPage.html', {'name': class_name})
+    course = Course.objects.filter(title=class_name)[0]
+    return render(request, 'classPage.html', {'course': course})
 
 def search_results(request):
     query = request.POST.get("query", "")
-    class_list = ['101','240','265']
+    class_list = retrieveObjects(request)
     return render(request, 'searchResults.html', {'query': query, 'class_list': class_list})
+
+def retrieveObjects(request):
+    query = request.POST.get("query", "")
+    softwareDev = Course(title="Software Dev")
+    entries = Course.objects.filter(title= query)
+    return entries
+
+
+
