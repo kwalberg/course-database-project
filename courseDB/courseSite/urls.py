91ed8cd7d592e5ae970edd85405d159aede9caa4
kwalberg <kai.walberg@gmail.com>
2018-03-29 18:30:39 -0500

    Added login and signup pages

--- courseDB/courseSite/urls.py
+++ courseDB/courseSite/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url, include
 from django_filters.views import FilterView
 
 
 urlpatterns = [
-    url(r'^search/$', FilterView.as_view(template_name='search/user_list.html'), name='search'),
+    url('search/', FilterView.as_view(template_name='search/user_list.html'), name='search'),
     path('', views.search, name='search'),
     path('results/', views.search_results, name='results'),
     path('class/<str:class_name>/', views.class_page, name='class'),
-    path('login/', views.login, name='login'),
+    url(r'^login/$', auth_views.login, {"template_name": "Login.html"}),
     path('rate/<str:class_name>/', views.rate_course, name='rate'),
-    path('submit_review/<str:class_name>/', views.submit_review, name='submit_review')
+    path('submit_review/<str:class_name>/', views.submit_review, name='submit_review'),
+    url('signup/', views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- courseDB/courseSite/views.py
+++ courseDB/courseSite/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Course
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def search(request):
@@ -39,4 +42,18 @@ def rate_course(request, class_name):
 
 def login(request):
     return render(request, 'Login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('search')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
