5b0dbe8356dd210246f9638c052e076671532c27
kwalberg <kai.walberg@gmail.com>
2018-04-04 15:40:14 -0500

    Updated course model

--- courseDB/courseSite/models.py
+++ courseDB/courseSite/models.py
@@ -9,10 +9,6 @@ class Course(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     instructor = models.CharField(max_length=150)
-    DURATION_CHOICES = [('1', '2 Weeks'), ('2', '8 Weeks')]
-    duration = models.CharField(max_length=7, choices=DURATION_CHOICES)
-    # Don't require file upload
-    # image = models.ImageField('course_art/', null=True, blank=True)
 
     def __str__(self):
         return self.title
