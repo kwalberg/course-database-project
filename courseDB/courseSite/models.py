d855162634947b46af625cbb77cc205afbdda2b7
kwalberg <kai.walberg@gmail.com>
2018-04-11 16:32:02 -0500

    Added more fields to course model

--- courseDB/courseSite/models.py
+++ courseDB/courseSite/models.py
@@ -6,9 +6,15 @@ from django.contrib.auth.models import User
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=250)
+    # Ex: COMP 225-01
+    course_id = models.CharField(max_length=20)
+    full_title = models.CharField(max_length=250)
+    course_number = models.IntegerField()
+    department = models.CharField(max_length=5)
     description = models.TextField()
     instructor = models.CharField(max_length=150)
+    days = models.CharField(max_length=5)
+    times = models.CharField(max_length=10)
 
     def __str__(self):
         return self.title
